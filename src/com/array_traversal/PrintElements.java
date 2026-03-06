243837df68bcd9c00ba42e5462f6e790ae466d45
abhijitholey <160751052+abhijitholey@users.noreply.github.com>
2026-03-06 17:27:10 +0530

    print array traversal

---
+++ src/com/array_traversal/PrintElements.java
@@ -0,0 +1,14 @@
+package com.array_traversal;
+
+public class PrintElements {
+
+	public static void main(String[] args) {
+		int arr[]= {11,12,13,14,15,16,17,18};
+		//for loop to print all elements
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
