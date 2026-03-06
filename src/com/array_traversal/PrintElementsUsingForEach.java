f06d925e8a1710dbabb9e3a55fd6201156abad31
abhijitholey <160751052+abhijitholey@users.noreply.github.com>
2026-03-06 17:49:55 +0530

    Print Elements using for each

---
+++ src/com/array_traversal/PrintElementsUsingForEach.java
@@ -0,0 +1,14 @@
+package com.array_traversal;
+
+public class PrintElementsUsingForEach {
+
+	public static void main(String[] args) {
+		int arr[]= {25,26,27,28,29,30,31};
+	    System.out.println("The elements of array are: ");
+	    for(int n:arr) {
+	    	System.out.println(n);
+	    }
+
+	}
+
+}
